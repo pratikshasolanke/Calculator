ffb5bf18920f946c566543782906741eb7b480a5
pratikshasolanke <57934123+pratikshasolanke@users.noreply.github.com>
2020-01-22 17:10:19 +0530

    Add the  Percentage program

--- cal.py
+++ cal.py
@@ -29,3 +29,19 @@ elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
 else:
    print("Invalid input")
+   
+   
+ #Percentage,Average calculation program  
+ english = float(input("Please enter English Marks: "))
+math = float(input("Please enter Math score: "))
+computers = float(input("Please enter Computer Marks: "))
+physics = float(input("Please enter Physics Marks: "))
+chemistry = float(input("Please enter Chemistry Marks: "))
+
+total = english + math + computers + physics + chemistry
+average = total / 5
+percentage = (total / 500) * 100
+
+print("\nTotal Marks = %.2f"  %total)
+print("Average Marks = %.2f"  %average)
+print("Marks Percentage = %.2f"  %percentage)
